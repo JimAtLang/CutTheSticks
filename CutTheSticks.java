c2391622bd97cfce78cba4870eccb11e93d4d16f
jimatlang <jim.stewart@thelangschool.org>
2024-02-02 09:41:13 -0500

    start project

---
+++ CutTheSticks.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class CutTheSticks {
+    public int cut(ArrayList<Integer> sticks){
+        return 0;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+    Scanner sc;
+    public static void main(String[] args){
+        new Main();
+    }
+
+    public Main(){
+        sc = new Scanner(System.in);
+        int numSticks = sc.nextInt();
+        ArrayList<Integer> sticks = new ArrayList<>();
+        for(int i=0;i<numSticks; i++){
+            sticks.add(sc.nextInt());
+        }
+    }
+}
