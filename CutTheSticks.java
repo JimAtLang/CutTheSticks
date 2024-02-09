82b901bc012696932bf57a15c45ddd24100ad940
Conrad Thompson <conrad.thompson@thelangschool.net>
2024-02-09 10:33:15 -0500

    cut function shortest stick

--- CutTheSticks.java
+++ CutTheSticks.java
@@ -3,7 +3,14 @@ import java.util.ArrayList;
 public class CutTheSticks {
     public ArrayList<Integer> cut(ArrayList<Integer> sticks){
         ArrayList<Integer> rtrn = new ArrayList<>();
-
+        int numSticks = sticks.size();
+        int shortest = sticks.get(index:0);
+        for(int stick:sticks){
+            if(shortest<stick){
+                stick = shortest;
+            }
+        }
+        System.out.println("The shortest stick is + ")
         return rtrn;
     }
 }
